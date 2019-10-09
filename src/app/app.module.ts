6f152253b5a12652062c58b41a791aa2c086040c
aliabbas18 <56329509+aliabbas18@users.noreply.github.com>
2019-10-08 22:59:09 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,15 @@
-import
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+@NgModule({
+  imports: [
+      BroswerModule
+  ],
+  declarations: [
+      AppComponent
+  ],
+  bootstrap: [
+      AppComponent
+  ]
+})
+export class AppModule{}
